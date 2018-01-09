72df15759ab1af35a10f1be20716e3ad97f2c8ce
Boris Rebolledo-Jaramillo <berebolledo@gmail.com>
2018-01-09 17:29:20 -0300

    bqsr

--- gatk_bqsr.sh
+++ gatk_bqsr.sh
@@ -8,20 +8,67 @@
 
 source $HOME/.bashrc
 
-input=${1}
-chrom=${2}
-refdata="/hpcudd/ICIM/shared/genomes/Homo_sapiens/Ensembl/GRCh37"
+set -e
+set -u
+set -o pipefail
+
+
+ if [ $HOSTNAME == 'sofia.udd.cl' ] || [[ $HOSTNAME == compute-1-*.local ]]
+then
+    genomes="/hpcudd/ICIM/shared/genomes"
+    bundle="/hpcudd/ICIM/shared/gatk-bundle"
+elif [ $HOSTNAME == 'mendel' ]
+then
+    genomes="/storage/shared/references"
+    bundle="/storage/shared/gatk-bundle"
+else
+    echo "Unrecognized host $HOSTNAME"
+    echo "can't locate genome references"
+    exit 1
+fi
+
+while getopts 'i:c:h' ARGS; do
+	case "$ARGS" in
+        i)
+          input="$OPTARG"
+          ;;
+        c)
+          chrom="$OPTARG"
+          ;;
+        h)
+          echo "script usage: $(basename $0) [-i input.bam] [-c chromosome N|ALL]" >&2
+          exit 0
+          ;;
+        ?)
+          echo "script usage: $(basename $0) [-i input.bam] [-c chromosome N|ALL]" >&2
+          exit 1
+          ;;
+    esac
+done
+    
+shift "$(($OPTIND - 1))"
+
+refdata="${genomes}/Homo_sapiens/Ensembl/GRCh37"
 genome="${refdata}/Sequence/WholeGenomeFasta/genome.fa"
-bundle="/hpcudd/ICIM/shared/gatk-bundle"
 dbsnp="${bundle}/b37/dbsnp_138.b37.vcf.gz"
 indels="${bundle}/b37/Mills_and_1000G_gold_standard.indels.b37.vcf.gz"
 
-gatk -Xms4g -Xmx8g \
-    -T BaseRecalibrator \
-    -R $genome \
-    -I $input \
-    -L $chrom \
-    -knownSites $dbsnp \
-    -knownSites $indels \
-    -o ${input}_${chrom}_recal_data.table
-
+if [ $chrom == "ALL" ]
+then
+    gatk -Xms4g -Xmx8g \
+        -T BaseRecalibrator \
+        -R $genome \
+        -I $input \
+        -knownSites $dbsnp \
+        -knownSites $indels \
+        -o ${input}_${chrom}_recal_data.table
+else
+    gatk -Xms4g -Xmx8g \
+        -T BaseRecalibrator \
+        -R $genome \
+        -I $input \
+        -L $chrom \
+        -knownSites $dbsnp \
+        -knownSites $indels \
+        -o ${input}_${chrom}_recal_data.table
+fi
