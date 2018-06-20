190649446e5d70d18b0a8ef0852c37f2f5b7a8d7
Boris Rebolledo <boris@bioinformagic.local>
2018-06-20 12:11:46 -0400

    NM-tag evaluation

--- ngs_stats/sam_tag_distribution.py
+++ ngs_stats/sam_tag_distribution.py
@@ -1,1 +1,34 @@
 #! /bin/env python
+
+import pysam
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+#sys.argv = [0, "test.bam"]
+
+samfile = pysam.AlignmentFile(sys.argv[1], "rb")
+
+notag = 0
+value = list()
+
+for read in samfile:
+	try:
+		# Get NM value
+		value.append(read.get_tag("NM"))
+	except:
+		# Count reads with no NM tag
+		notag+=1
+
+#print notag
+#print value
+
+data = np.array(value)
+plt.hist(data, weights=np.zeros_like(data) + 1. / data.size ) 
+plt.text(4,0.5, "N reads = %d" % (notag+len(value)))
+plt.text(4,0.45, "No tag = %d" % notag)
+plt.text(4,0.4, "min = %d | max = %d | median = %d" % (np.min(value), np.max(value), np.median(value)))
+plt.text(4,0.35, "Q.25 = %1.f | Q.75 = %.1f | Q.90 = %.1f" % (np.percentile(value,25), np.percentile(value,75), np.percentile(value,90) ) )
+plt.ylim([0,1])
+
+plt.savefig("NM-tag_distribution_%s.pdf" % sys.argv[1])
